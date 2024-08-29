07fe59c5216c4f68ac4b24dc252a1fbf5924f638
gonzageraci <gonza.geraci@gmail.com>
2024-08-29 15:30:38 -0300

    chore: Update repository URL in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Un toolkit Backend para FastAPI",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/gonzageraci/FastAPI-toolkit",
+    url="https://github.com/gonzageraci/Zetta-Backend-Toolkit",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
