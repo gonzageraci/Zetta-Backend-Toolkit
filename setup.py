02a45929b56ab294ae6fd9d4b7cd92f891f82637
gonzageraci <gonza.geraci@gmail.com>
2024-08-29 14:55:58 -0300

    chore: Rename package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="fastapi-toolkit",
+    name="zetta-backend-toolkit",
     version="0.1.0",
     author="Gonzalo Geraci",
     author_email="gonza.geraci@gmail.com",
