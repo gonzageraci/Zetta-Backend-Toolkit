443ba6174addb91940b69c45cc5515ffce64d74d
gonzageraci <gonza.geraci@gmail.com>
2024-08-29 15:55:46 -0300

    chore: Bump version to 0.1.3 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zetta-backend-toolkit",
-    version="0.1.2",
+    version="0.1.3",
     author="Gonzalo Geraci",
     author_email="gonza.geraci@gmail.com",
     description="Un toolkit Backend para FastAPI",
